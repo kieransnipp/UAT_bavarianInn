1a3c59cbf5fd678778d552ae8d4ee9e091c60840
Kieran <kieransnipp@gmail.com>
2019-07-19 10:58:23 +0100

    Demo test

---
+++ src/test/java/demo/PropertiesFile.java
@@ -0,0 +1,64 @@
+package demo;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.util.Properties;
+
+public class PropertiesFile {
+
+	public static String browser = null;
+	public static String url = null;
+	static Properties prop = new Properties();
+	static String projectPath = System.getProperty("user.dir");
+
+	public static void main(String[] args) throws IOException {
+		getProperties();
+		setProperties();
+	}
+
+	public static void getProperties() {
+		try {
+			InputStream input = new FileInputStream(projectPath + "/src/main/java/com/bav/config/config.properties");
+			prop.load(input);
+			browser = prop.getProperty("browser");
+			url = prop.getProperty("url");
+			System.out.println("Browser is = " + browser);
+			//TestNG_Demo.browserName = browser;
+//			TestNG_Demoa.browserName = browser;
+//			TestNG_Demoab.browserName = browser;	
+
+		} catch (Exception e) {
+			System.out.println("getMessage =" + e.getMessage());
+			System.out.println("getCause = " + e.getCause());
+
+		}
+		
+
+	} // End getProperties
+
+	public static void setProperties() throws IOException {
+		try {
+			OutputStream output = new FileOutputStream(projectPath + "/src/main/java/com/bav/config/config.properties");
+			prop.setProperty("result", "pass");
+			//prop.setProperty("browser", "chrome");
+			prop.store(output, "A comment");
+			
+			String browser = prop.getProperty("browser");
+			System.out.println("Browser is now updated to "+browser);
+		
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	} //End setProperties
+
+	
+	
+		
+}
---
+++ src/test/java/demo/TestNG_Demoa.java
@@ -0,0 +1,88 @@
+//Accesing the data VIA a static way
+package demo;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.message.LoggerNameAwareMessage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.codeborne.selenide.WebDriverRunner;
+
+public class TestNG_Demoa {
+
+	WebDriver driver = null;
+	static private Logger logger = LogManager.getLogger(TestNG_Demoa.class);
+	public static String browserName = null;
+
+	@BeforeTest
+	public void setUpTest() {
+		System.out.println("Second test running");
+		String projectPath = System.getProperty("user.dir");
+		PropertiesFile.getProperties();  //Sets the browserName for this class
+		
+		if (PropertiesFile.browser.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "/drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+		} else {
+			if (PropertiesFile.browser.equalsIgnoreCase("firefox")) {
+				System.out.println("Launching firefox");
+				// browserName = ((Object) driver).Firefox("drivers/geckodriver.exe");
+				System.setProperty("webdriver.gecko.driver", projectPath + "/drivers/geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+			WebDriverRunner.setWebDriver(driver);
+		}
+
+	} // End setUpTest
+
+	
+	@Test 
+	public void googleSearch() {
+		System.out.println("Get google");
+		driver.get("https://www.google.com/");
+		System.out.println("After google");
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String title = driver.getTitle();
+		
+		AssertJUnit.assertEquals("Google", title, "Google");
+		System.out.println("TestNG_Demoa Test Passed");
+//		driver.findElement(By.name("q")).sendKeys("Automation steps");
+//		driver.findElement(By.name("btnK")).click();
+	}
+	
+//	@Test
+//	public void firstTest() {
+//		System.out.println("Do you see this?");
+//	}
+	
+//	@AfterMethod
+//	public void afterMethod() {
+//		System.out.println("Before method");
+//	}
+	
+	@AfterSuite
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+		
+	}
+}
